a4879abd069c15d9f5fcea668b58f04db087a5f2
AdrianKauz <akauz@hispeed.ch>
2017-03-28 02:08:39 +0200

    Finished Uebung3_1
    At least it works. But no check if input values are correct.

--- Uebung3_1/main.c
+++ Uebung3_1/main.c
@@ -2,10 +2,11 @@
  * HSLU FS2017 - Mikrocontroller - TA.MC
  * Exercise 3.1
  * Get name and forename via console from the user and save the values
- * as a simple list
+ * as a simple list.
+ * Still full with bugs! Bleh!
  * @author Adrian Kauz
- * @version 1.0
- * @date 2017.03.27
+ * @version 0.5
+ * @date 2017.03.28
  * @return 0
  */
 
@@ -19,19 +20,22 @@ struct tPerson{
     char* forename;
     char* surname;
     struct tPerson* next;
+    struct tPerson* prev;
 };
 
-void fillListWithNames(struct tPerson* newList);
-//void printList(struct tPerson* newList);
+
+struct tPerson* fillListWithNames(struct tPerson* newList);
+void printList(struct tPerson* newList);
 char* readString(char* pText);
 struct tPerson* addElementToList(struct tPerson* currList, struct tPerson* newElement);
-struct tPerson* getNewElement(char* newUserInput, int* newUserInputLength);
+struct tPerson* generateNewElement(char* newUserInput, int* newUserInputLength);
+
 
 int main() {
     struct tPerson* list = NULL;
 
-    fillListWithNames(list);
-//printList(list);
+    system("cls");
+    printList(fillListWithNames(list));
 
     return(EXIT_SUCCESS);
 }
@@ -43,49 +47,54 @@ int main() {
  * @param Empty pointer to the list
  * @return Nothing
  */
-void fillListWithNames(struct tPerson* currList){
+struct tPerson* fillListWithNames(struct tPerson* currList){
     char* userInput;
     size_t stringLength;
 
+    printf("\nAdd a person. Forename first, surname second.\n"
+                   "Press <Enter> when finish.\n");
     do{
-        printf("\nAdd a person. forename first, surname second: ");
+        printf("--> " );
         userInput = readString(userInput);
         stringLength = strlen(userInput);
 
         if(stringLength > 0){
-            currList = addElementToList(currList, getNewElement(userInput, &stringLength));
-            printf("%s", currList -> forename);
+            currList = addElementToList(currList, generateNewElement(userInput, &stringLength));
         }
 
         free(userInput);
     }
     while(stringLength > 0);
 
-    return;
+    return currList;
 }
 
 
 /**
- * Reads input-stream from console. Memory is dynamically allocated.
+ * Adds new element into the linked list
  * @author Adrian Kauz
- * @param Empty Pointer
- * @return Pointer with all the characters from console
+ * @param Current list
+ * @param Pointer to new person-element
+ * @return Updated list
  */
 struct tPerson* addElementToList(struct tPerson* pCurrList, struct tPerson* pNewElement){
     if(pCurrList == NULL){
-        pCurrList = malloc(sizeof(struct tPerson));
-        *pCurrList = *pNewElement;
+        pCurrList = pNewElement;
+        pCurrList -> next = NULL;
+        pCurrList -> prev = NULL;
     }
     else{
-        struct tPerson* pTempElement = pCurrList;
+        // At first, find last element
+        struct tPerson* pCurrElement = pCurrList; // get first element from list
 
-        while(pTempElement -> next){
-            pTempElement = pTempElement -> next;
+        // Searching...
+        while(pCurrElement -> next != NULL){
+            pCurrElement = pCurrElement -> next;
         }
 
-        pTempElement -> next = pNewElement;
-
-        //free(tempElement);
+        // ...if found append new element to list
+        pCurrElement -> next = pNewElement;
+        pNewElement -> prev = pCurrElement;
     }
 
     return pCurrList;
@@ -93,15 +102,13 @@ struct tPerson* addElementToList(struct tPerson* pCurrList, struct tPerson* pNew
 
 
 /**
- * Convert users input into an person-struct.
+ * Convert user's input into a person-struct.
  * @author Adrian Kauz
  * @param Empty Pointer
  * @return Pointer with all the characters from console
  */
-struct tPerson* getNewElement(char* newUserInput, int* newUserInputLength){
-    struct tPerson* pNewPerson = malloc(sizeof(struct tPerson));
-
-    //Search for whitespace
+struct tPerson* generateNewElement(char* newUserInput, int* newUserInputLength){
+    // At first, search for whitespace...
     int lengthOfForename = 0;
 
     int x = 0;
@@ -112,9 +119,12 @@ struct tPerson* getNewElement(char* newUserInput, int* newUserInputLength){
         }
     }
 
+    // Then generate new element
+    struct tPerson* pNewPerson = malloc(sizeof(struct tPerson));
     pNewPerson -> forename = (char *) malloc(sizeof(char) * (lengthOfForename + 1));
     pNewPerson -> surname = (char *) malloc(sizeof(char) * (*newUserInputLength - lengthOfForename));
     pNewPerson -> next = NULL;
+    pNewPerson -> prev = NULL;
 
     int isPartOfForename = 1;
     for(x = 0 ; x <= *newUserInputLength ; x++){
@@ -168,4 +178,31 @@ char* readString(char* pText){
     }
 
     return pText;
+}
+
+
+/**
+ * Prints out the values of a struct-person-list
+ * @author Adrian Kauz
+ * @param List with struct-person elements
+ * @return Nothing
+ */
+void printList(struct tPerson* newList){
+    printf("\nResult of your input\n");
+    printf("--------------------");
+
+    struct tPerson* currElement = newList;
+
+    int validNextLoop = 1;
+    do{
+        if(currElement -> next == NULL){
+            validNextLoop = 0;
+        }
+
+        printf("\n- %s %s", currElement-> forename, currElement-> surname);
+        currElement = currElement -> next;
+    }
+    while(validNextLoop);
+
+    return;
 }
