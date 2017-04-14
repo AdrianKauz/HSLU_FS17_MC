42c7567423331b301be4ccc33ed5f215e1111437
AdrianKauz <akauz@hispeed.ch>
2017-04-14 14:13:09 +0200

    Start with 6.1

---
+++ Uebung6_1/Address.h
@@ -0,0 +1,8 @@
+//
+// Created by Adrian Kauz on 14.04.2017.
+//
+
+#ifndef UEBUNG6_1_ADDRESS_H
+#define UEBUNG6_1_ADDRESS_H
+
+#endif //UEBUNG6_1_ADDRESS_H
---
+++ Uebung6_1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "Address.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
